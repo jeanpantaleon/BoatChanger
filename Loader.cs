25b96a3d5220b6d0360f8e6cc8a9f8d3b280bb80
Jean Pantaléon <78849811+SeaKestrel@users.noreply.github.com>
2024-04-21 22:32:05 +0200

    v1.0.0

---
+++ Loader.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BoatChanger
+{
+	public class Loader
+	{
+		/// <summary>
+		/// This method is run by Winch to initialize your mod
+		/// </summary>
+		public static void Initialize()
+		{
+			var gameObject = new GameObject(nameof(BoatChanger));
+			gameObject.AddComponent<BoatChanger>();
+            GameObject.DontDestroyOnLoad(gameObject);
+			GameManager.Instance._prodGameConfigData.hourDurationInSeconds = 24;
+            new Harmony("com.dredge.moredredge").PatchAll();
+        }
+	}
+}
